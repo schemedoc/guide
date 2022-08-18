4d5a5525c47413270512317de15c1acbfae2b67b
Lassi Kortela <lassi@lassi.io>
2022-08-18 10:21:00 +0300

    Adjust paths to match rakefile

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 cd "$(dirname "$0")"
-rsync -crv www/ alpha.servers.scheme.org:/production/doc/www/guide/
+rsync -crv www/ alpha.servers.scheme.org:/production/docs/www/
--- www.sh
+++ www.sh
@@ -3,9 +3,10 @@ set -eu -o pipefail
 cd "$(dirname "$0")"
 mkdir -p www
 find www/ -mindepth 1 -delete
+mkdir www/guide
 for adoc in $(git ls-files | grep -F .adoc); do
     stem="$(echo "$adoc" | sed 's@\.adoc$@@')"
-    hdir="www/$stem"
+    hdir="www/guide/$stem"
     html="$hdir/index.html"
     echo "$html"
     asciidoctor -b html5 -o "$html" "$adoc"
